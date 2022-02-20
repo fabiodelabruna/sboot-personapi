fa31a045b45c61fcf10ce5a415d0d9c92e3707ee
Fabio Dela Bruna <fabio.bruna@agrotis.com>
2022-02-20 19:15:04 -0300

    Desenvolvido a funcionalidade de excluir uma pessoa pelo ID

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.personapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,6 +35,12 @@ public class PersonController {
     @ResponseStatus(value = HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid final PersonDTO personDTO) {
         return personService.createPerson(personDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable final Long id) throws PersonNotFoundException {
+        personService.deleteById(id);
     }
 
     @GetMapping
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -31,14 +31,23 @@ public class PersonService {
         return MessageResponseDTO.builder().message("Created person with ID " + savedPerson.getId()).build();
     }
 
+    public void deleteById(final Long id) throws PersonNotFoundException {
+        verifyIfExistis(id);
+        personRepository.deleteById(id);
+    }
+
     public List<PersonDTO> listAll() {
         final List<Person> all = personRepository.findAll();
         return all.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 
     public PersonDTO findById(final Long id) throws PersonNotFoundException {
-        final Person person = personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+        final Person person = verifyIfExistis(id);
         return personMapper.toDTO(person);
+    }
+
+    private Person verifyIfExistis(final Long id) throws PersonNotFoundException {
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
     }
 
 }
